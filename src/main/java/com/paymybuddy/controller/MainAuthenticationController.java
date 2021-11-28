1a50bc3cd2ed93a2ea2fc484ac1add9eecbc181e
chyadr <chaimaafakir@gmail.com>
2021-11-28 23:18:45 +0100

    [OPC][Projet6: update Readme- update Bank-transfer.html- apdate expire session into 10min]

--- src/main/java/com/paymybuddy/controller/MainAuthenticationController.java
+++ src/main/java/com/paymybuddy/controller/MainAuthenticationController.java
@@ -13,7 +13,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import org.springframework.stereotype.Controller;
